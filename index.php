d980c9ff7a9fd6cdd1f9c43dd1ceee281669910b
mah.shamim <mah.shamim@gmail.com>
2022-08-27 01:10:48 +0600

    initial php setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+    // Using two forward slashes we create comments in php.
+    // Within the php tag, we can insert values into our HTML.
+    echo "<h1>Welcome!</h1>";
+    ?>
+  </body>
+</html>
