ea07ecf0cf10b12de8b7ce9e3cbbcac38fcae3ed
mah.shamim <mah.shamim@gmail.com>
2022-08-27 01:14:11 +0600

    add foreach loop

--- index.php
+++ index.php
@@ -5,18 +5,21 @@
   </head>
   <body>
     <?php
-      $name = "PHP Store";
-      $credit = 1000; 
+	    $name = "PHP Store";
+			$credit = 1000; 
 
-      echo "<h1>Welcome to ".$name."!</h1>";
-      echo "<h2>You have $".$credit." in your wallet.</h2>";
+	    echo "<h1>Welcome to ".$name."!</h1>";
+	    echo "<h2>You have $".$credit." in your wallet.</h2>";
 
-	    $products['Computer']=750;
+		  $products['Computer']=750;
 	    $products['Car']=15000;
 	    $products['iPhone']=1000;
 	    $products['Toaster']=75;
 
-	    echo "<p>A car costs $".$products['Car']."</p>";
+	    foreach($products as $key => $value){
+		    echo "<p>The ".$key." costs ".$value."</p>";
+	    }
+
     ?>
   </body>
 </html>
