783572b13774358b323b0a30bb362b94d6e63080
mah.shamim <mah.shamim@gmail.com>
2022-08-27 01:12:14 +0600

    A car costs 5000

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 
       echo "<h1>Welcome to ".$name."!</h1>";
       echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+	    $products['Computer']=750;
+	    $products['Car']=15000;
+	    $products['iPhone']=1000;
+	    $products['Toaster']=75;
+
+	    echo "<p>A car costs $".$products['Car']."</p>";
     ?>
   </body>
 </html>
